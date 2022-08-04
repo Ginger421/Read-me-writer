ebb830375564c07afbf399ca9cdbb64b501cb3bc
Ginger Owen <gingerowen0421@gmail.com>
2022-08-04 13:17:35 -0600

    mostly complete file

--- index.js
+++ index.js
@@ -1,1 +1,115 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = 
+  [
+    {
+      type: 'input',
+      message: "What is the project's title?",
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: "Please give a description of the project.",
+      name: 'description',
+    },
+    // {
+    //   type: 'input',
+    //   message: 'Please enter the Table of Contents',
+    //   name: 'table',
+    // },
+    {
+      type: 'input',
+      message: 'Please enter the installation instructions',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'Please enter the usage information',
+      name: 'usage',
+    },
+      // {
+      //   type: 'checkbox',
+      //   choices: [ ''],
+      //   name: 'checkbox',
+      // },  
+    {
+      type: 'input',
+      message: 'How can other developers contribute to this project?',
+      name: 'contributing',
+    },
+    {
+      type: 'input',
+      message: 'How can other ',
+      name: 'testing',
+    },
+    {
+      type: 'input',
+      message: 'What is your email address?',
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message: 'What is your GitHub user name?',
+      name: 'github',
+    }
+  ]
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions)
+  .then((response) => {
+    console.log(response)
+    const markdown = `
+# ${response.title}
+
+## Description
+${response.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#)
+* [Testing](#testing)
+* [Questions](#questions)
+
+## Installation 
+${response.installation}
+
+## Usage
+${response.usage}
+
+## License
+
+## How to Contribute 
+${response.contributing}
+
+## Testing 
+${response.testing}
+
+## Questions
+${response.email}
+${response.github}`;
+
+console.log(markdown);
+
+writeFile('README.md', markdown);
+    }
+  );
+
+ 
+
+}
+
+// TODO: Create a function to write README file
+function writeFile(fileName, markdown) {
+  fs.writeFile(fileName, markdown, (err) =>
+  err ? console.log(err) : console.log('File created!'))
+};
+
+
+// Function call to initialize app
+init();
 
